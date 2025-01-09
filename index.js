2f74d03e5be7b58d2da14102364acee1e378eaae
Mst <bilkis.naher@yahoo.co.uk>
2025-01-09 09:39:51 +0000

    add img and code

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice) {
-  const computerChoice = choices[Math.floor(Math.random() * 3) ];
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
   let result = "";
 
   if (playerChoice === computerChoice) {
@@ -14,13 +19,13 @@ function playGame(playerChoice) {
   } else {
     switch (playerChoice) {
       case "rock":
-        result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE";
+        result = computerChoice === "scissors" ? "YOU WIN!" : "YOU LOSE";
         break;
       case "paper":
-        result = (computer === "rock") ? "YOU WIN!" : "YOU LOSE";
+        result = computer === "rock" ? "YOU WIN!" : "YOU LOSE";
         break;
       case "secissors":
-        result = (computer === "paper") ? "YOU WIN" : "YOU LOSE";
+        result = computer === "paper" ? "YOU WIN" : "YOU LOSE";
         break;
     }
   }
@@ -29,15 +34,18 @@ function playGame(playerChoice) {
   computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
   resultDisplay.textContent = result;
 
-resultDisplay.classList.remove("greenText", "redText");
+  resultDisplay.classList.remove("greenText", "redText");
 
-switch(result){
-case "YOU WIN!":
-    resultDisplay.classList.add("greenText");
-    break;
+  switch (result) {
+    case "YOU WIN!":
+      resultDisplay.classList.add("greenText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
     case "YOU LOSE":
-        resultDisplay.classList.add("redText");
-        break;
-}
-
+      resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
 }
